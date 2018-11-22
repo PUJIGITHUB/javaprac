c65a890a744e8d9dd12d3eced550a20f257faced
pooja.rathore <pooja.rathore@NBEC-INA01908S1.necisw.in>
2018-11-22 18:27:47 +0530

    New

---
+++ src/com/pract/java/Dulic.java
@@ -0,0 +1,35 @@
+package com.pract.java;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Dulic {
+
+	public static void main(String[] args) {
+
+		int[] ArrayNum = { 10 , 20 , 51 , 12 ,  10 ,  46 ,  51 ,  51 , 20 };
+		duplicate(ArrayNum);
+	}
+
+	static void duplicate(int num[]) {  
+		Map<Integer , Integer> mapvalue = new HashMap<Integer , Integer>();
+
+		for(int key: num){
+
+			if (mapvalue.containsKey(key)) {  
+				mapvalue.put(key, mapvalue.get(key)+1);
+			} 
+			else {
+				mapvalue.put(key, 1);  
+			} 
+		}
+		//System.out.println(mapvalue);
+
+		for (Map.Entry<Integer,Integer> entry : mapvalue.entrySet())  {
+			if(entry.getValue()>1){
+				System.out.println("Key = " + entry.getKey() + 
+						", Value = " + entry.getValue()); 
+			}                    
+		} 
+	}
+}
